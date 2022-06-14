a668e700572a0ec9300546816d5bf5bb549b653a
Matt Hinz <matt.hinz@gsa.gov>
2022-06-14 09:10:22 -0700

    Fix ProcessList inner blocks
    Use `useInnerBlocksProps` rather than `<InnerBlocks>` to avoid extra `<div>` markup

--- src/Editor/blocks/ProcessList/edit.js
+++ src/Editor/blocks/ProcessList/edit.js
@@ -11,17 +11,15 @@ export function ProcessListEditor() {
   const blockProps = useBlockProps({
     className: "usa-process-list",
   });
-  const innerBlocksProps = useInnerBlocksProps(blockProps, {});
+  const innerBlocksProps = useInnerBlocksProps(blockProps, {
+    allowedBlocks: ["uswds/process-list-item"],
+    renderAppender: false,
+    template: TEMPLATE,
+  });
 
   return (
     <div {...blockProps}>
-      <ol className="usa-process-list">
-        <InnerBlocks
-          allowedBlocks={["uswds/process-list-item"]}
-          renderAppender={false}
-          template={TEMPLATE}
-        />
-      </ol>
+      <ol className="usa-process-list" {...innerBlocksProps}></ol>
       <InnerBlocks.ButtonBlockAppender />
     </div>
   );
